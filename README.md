0421032551be62060c7d1c40893032c48f88df82
lukas-dachtler <software@lukas-dachtler.de>
2019-03-12 21:36:06 +0100

    v 2.0

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 #### pwned.py is a small script to check if your password has been pwned by checking the API from [haveibeenpwned.com](https://haveibeenpwned.com/API/v2)
 ## ■ Install
 Install [Requests](http://docs.python-requests.org/en/v2.7.0/user/install/)
-## ■ Use
+## ■ Use (single password)
 ```
 python pwned.py <your password>
 ```
+## ■ Use (file)
+```
+python pwned.py <path\filename.txt>
+```
+> **Format:** Each password has to be in a single line
+
+> **Result:** Results will be written next to each password in the same file
--- pwned.py
+++ pwned.py
@@ -1,20 +1,44 @@
 import sys
 import hashlib
 import requests
-password_found = ''
-hash_generator = hashlib.sha1()
-hash_generator.update(sys.argv[1].encode('utf-8'))
-hash_str = hash_generator.hexdigest().upper()
-hash_prefix = hash_str[:5]
-hash_suffix = hash_str[5:]
-req = requests.get('https://api.pwnedpasswords.com/range/' + hash_prefix)
-passwords_all = req.text.split()
-for p in passwords_all:
-	if hash_suffix in p:
-		password_found = p
-		break
-if password_found:
-	print ('Password found ' + password_found.split(':')[1] + ' times!')
-	print ('Hash: ' + hash_str)
+import os.path
+
+def check_password(password):
+	password_found = ''
+	hash_generator = hashlib.sha1()
+	hash_generator.update(password)
+	hash_str = hash_generator.hexdigest().upper()
+	hash_prefix = hash_str[:5]
+	hash_suffix = hash_str[5:]
+	req = requests.get('https://api.pwnedpasswords.com/range/' + hash_prefix)
+	passwords_result = req.text.split()
+	for p in passwords_result:
+		if hash_suffix in p:
+			return p
+	return ''
+
+input_str = sys.argv[1]
+if '.txt' in input_str:
+	if os.path.isfile(input_str):
+		with open(input_str, 'r') as file:
+			passwords_check = file.readlines()
+		i = 0
+		while i < len(passwords_check):
+			passwords_check[i] = passwords_check[i].rstrip()
+			result = check_password(passwords_check[i].encode('utf-8'))
+			if result:
+				passwords_check[i] += " - found " + result.split(':')[1] + ' times!\n'
+			else:
+				passwords_check[i] += " - not found!\n"
+			i += 1
+		with open(input_str, 'w') as file:
+			file.writelines(passwords_check)
+		print ('Completed!')
+	else:
+		print ('Path not valid!')
 else:
-	print ('Password not found!')
+	result = check_password(input_str.encode('utf-8'))
+	if result:
+		print ('Password found ' + result.split(':')[1] + ' times!')
+	else:
+		print ('Password not found!')
